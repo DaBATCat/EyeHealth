9816aa08e2bbe1883aecdd20c717a2335d92ff1d
Daniel <daniel.dreiling@gmx.de>
2022-08-09 00:38:41 +0200

    first commit

---
+++ EyeHealth/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EyeHealth
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Test
+            //Test 2
+            //Test 3
+        }
+    }
+}
