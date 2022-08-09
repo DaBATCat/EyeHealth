b1f0b93aa49e5cd56f9c0c0ea78e33947f0abc2f
Daniel <daniel.dreiling@gmx.de>
2022-08-09 23:11:28 +0200

    Added classes

---
+++ EyeHealth/InternalTools.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EyeHealth
+{
+    internal class InternalTools
+    {
+        public int SecondsInMiliseconds(int seconds) => seconds * 1000;
+    }
+}
